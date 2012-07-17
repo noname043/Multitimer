d84a89dca17c3a99fd3d4102cd672b8d3b3dc02e
noname043 <noname043@o2.pl>
2012-07-17 23:17:27 +0200

    Nah. Soft keys fixed.

--- src/multitimer.cpp
+++ src/multitimer.cpp
@@ -25,8 +25,8 @@ Multitimer::Multitimer(QWidget *parent):
     addAction(_quitAction);
     addAction(_optionsAction);
 #ifndef S60V5
-    _quitAction->setIcon(qApp->style()->standardPixmap(static_cast<QStyle::StandardPixmap>(SP_CustomToolBarBack)));
-    _optionsAction->setIcon(qApp->style()->standardPixmap(QStyle::SP_TitleBarMenuButton));
+    _quitAction->setIcon(qApp->style()->standardIcon(static_cast<QStyle::StandardPixmap>(SP_CustomToolBarBack)));
+    _optionsAction->setIcon(qApp->style()->standardIcon(QStyle::SP_TitleBarMenuButton));
 #endif
     _quitAction->setSoftKeyRole(QAction::NegativeSoftKey);
     _optionsAction->setSoftKeyRole(QAction::PositiveSoftKey);
